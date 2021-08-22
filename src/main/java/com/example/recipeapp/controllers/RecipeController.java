1ec742a9e559bc5ad15d8da65118217568cea8af
rbaliwal00 <rajan18baliwal@gmail.com>
2021-08-22 14:33:26 +0530

    delete a recipe

--- src/main/java/com/example/recipeapp/controllers/RecipeController.java
+++ src/main/java/com/example/recipeapp/controllers/RecipeController.java
@@ -4,10 +4,7 @@ import com.example.recipeapp.commands.RecipeCommand;
 import com.example.recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by jt on 6/19/17.
@@ -45,6 +42,14 @@ public class RecipeController {
     public String updateRecipe(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
         return  "recipe/recipeform";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{id}/delete")
+    public String deleteById(@PathVariable String id){
+
+        recipeService.deleteById(Long.valueOf(id));
+        return "redirect:/";
     }
 
 }
--- src/main/java/com/example/recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/example/recipeapp/services/RecipeServiceImpl.java
@@ -63,4 +63,8 @@ public class RecipeServiceImpl implements RecipeService {
         return recipeToRecipeCommand.convert(savedRecipe);
     }
 
+    @Override
+    public void deleteById(Long idToDelete) {
+        recipeRepository.deleteById(idToDelete);
+    }
 }

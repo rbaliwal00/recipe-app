51c2ba239943b87ef22af947545a6402e3afc318
rbaliwal00 <rajan18baliwal@gmail.com>
2021-08-22 15:22:00 +0530

    implemented view and show ingredient

---
+++ src/main/java/com/example/recipeapp/services/IngredientService.java
@@ -0,0 +1,7 @@
+package com.example.recipeapp.services;
+
+import com.example.recipeapp.commands.IngredientCommand;
+
+public interface IngredientService {
+    IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+}

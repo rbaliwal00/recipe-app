4058d09864b8fb1e0bb40135165d6ed6e2d2278b
rbaliwal00 <rajan18baliwal@gmail.com>
2021-08-23 18:02:47 +0530

    create ingredient implemented

--- src/main/java/com/example/recipeapp/controllers/IngredientController.java
+++ src/main/java/com/example/recipeapp/controllers/IngredientController.java
@@ -1,6 +1,8 @@
 package com.example.recipeapp.controllers;
 
 import com.example.recipeapp.commands.IngredientCommand;
+import com.example.recipeapp.commands.RecipeCommand;
+import com.example.recipeapp.commands.UnitOfMeasureCommand;
 import com.example.recipeapp.services.IngredientService;
 import com.example.recipeapp.services.RecipeService;
 import com.example.recipeapp.services.UnitOfMeasureService;
@@ -51,6 +53,28 @@ public class IngredientController {
         model.addAttribute("uomList", unitOfMeasureService.listAllUoms());
         return "recipe/ingredient/ingredientform";
     }
+
+    @GetMapping
+    @RequestMapping("recipe/{recipeId}/ingredient/new")
+    public String newIngredient(@PathVariable String recipeId, Model model){
+
+        //make sure we have a good id value
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(recipeId));
+        //todo raise exception if null
+
+        //need to return back parent id for hidden form property
+        IngredientCommand ingredientCommand = new IngredientCommand();
+        ingredientCommand.setRecipeId(Long.valueOf(recipeId));
+        model.addAttribute("ingredient", ingredientCommand);
+
+        //init uom
+        ingredientCommand.setUom(new UnitOfMeasureCommand());
+
+        model.addAttribute("uomList",  unitOfMeasureService.listAllUoms());
+
+        return "recipe/ingredient/ingredientform";
+    }
+
 
     @PostMapping("recipe/{recipeId}/ingredient")
     public String saveOrUpdate(@ModelAttribute IngredientCommand command){

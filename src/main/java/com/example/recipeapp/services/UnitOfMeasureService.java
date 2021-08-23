bfc0b57f58192d0e13e594afac2c20eed55cf42b
rbaliwal00 <rajan18baliwal@gmail.com>
2021-08-23 17:48:30 +0530

    update ingredient implemented

---
+++ src/main/java/com/example/recipeapp/services/UnitOfMeasureService.java
@@ -0,0 +1,9 @@
+package com.example.recipeapp.services;
+
+import com.example.recipeapp.commands.UnitOfMeasureCommand;
+
+import java.util.Set;
+
+public interface UnitOfMeasureService {
+    Set<UnitOfMeasureCommand> listAllUoms();
+}

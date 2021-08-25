74825cedac36410f1888603ca0936a2f2b9fbc69
rbaliwal00 <rajan18baliwal@gmail.com>
2021-08-25 21:23:01 +0530

    updated ImageController

--- src/main/java/com/example/recipeapp/repostitories/RecipeRepository.java
+++ src/main/java/com/example/recipeapp/repostitories/RecipeRepository.java
@@ -3,6 +3,8 @@ package com.example.recipeapp.repostitories;
 import com.example.recipeapp.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 
 }
